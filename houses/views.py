71be15478c28542df8af72598b048794bccd66a3
Nick <kizrumscience@yandex.ru>
2021-04-10 16:24:31 +0300

    v 2.0.0 Сосздана admin, возможность управлять ею. Вывод на страницу информации о домах

--- houses/views.py
+++ houses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from houses.models import House
 
-# Create your views here.
+
+def houses_list(request):
+
+    houses = House.objects.all() # создаем запрос через ORM, в переменной houses содержаться все дома,
+    # которые добавили через админку, эквивалентно SELECT * FROM houses_house ORDER BY name, через модель House
+    # получаем все объекты, queryset - набор данных полученных с помощью SQL запроса
+    for house in houses:
+        print(house.name, house.price)
+    return render(request, "houses/houses_list.html", {"houses": houses})
