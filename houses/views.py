0b5535f2b1ae93ff7bb78425153ff0eb4ee59527
Nick <kizrumscience@yandex.ru>
2021-05-01 15:54:58 +0300

    v 2.2.0 Создана заявка для покупки дома

--- houses/views.py
+++ houses/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from houses.models import House
-from django.http import HttpResponse
+from orders.forms import OrderForm
 
 
 def houses_list(request):
-
     houses = House.objects.all()  # создаем запрос через ORM, в переменной houses содержаться все дома,
     # которые добавили через админку, эквивалентно SELECT * FROM houses_house ORDER BY name, через модель House
     # получаем все объекты, queryset - набор данных полученных с помощью SQL запроса
@@ -15,17 +14,9 @@ def houses_list(request):
 
 def house_detail(request, house_id):
     house = get_object_or_404(House, id=house_id)
-    print(house.name)
-    return render(request, "houses/house_detail.html", {"house": house})
+    form = OrderForm(request.POST or None, initial={"house": house})
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+    return render(request, "houses/house_detail.html", {"house": house, "form": form})
 
-
-# def about_about(request, house_name):
-#     """Опиание дома в отдельной странице"""
-#     house = House.objects.get(name=house_name)
-#     output1 = f"<h1><b>{house_name}</b></h1>"
-#     output2 = f'<h1><br><b>Описание</b></h1> <b>{house_name}</b> продается по цене {house.price}$<br>{house.description}'
-#     if house.photo:
-#         return HttpResponse(output1 + f'<img src="{house.photo.url}" alt="{house.name }" width="640" height="480"> <br>'
-#                             + output2)
-#     else:
-#         return HttpResponse(output1 + output2)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from houses.models import House
 
-# Create your models here.
+
+class Order(models.Model):
+    house = models.ForeignKey(House, verbose_name="дом", on_delete=models.CASCADE)
+    name = models.CharField("имя", max_length=50)
+    phone = models.CharField("телефон", max_length=50)
+    date = models.DateField("дата", auto_now_add=True)
+
+    class Meta:
+        # описательный класс
+        verbose_name = "заявка"
+        verbose_name_plural = "заявки"
+        # сортировка
+        ordering = ["date", "name"]
+
+    def __str__(self):
+        # houses.object = name in up admin
+        return self.name
