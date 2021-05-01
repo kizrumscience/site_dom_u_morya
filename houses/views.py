d0c6f3b858a994f99df9db3c68c4ac53b4fb12b7
Nick <kizrumscience@yandex.ru>
2021-05-01 16:55:50 +0300

    v 2.4.0 Создан фильтр по ценам

--- houses/views.py
+++ houses/views.py
@@ -4,15 +4,22 @@ from django.urls import reverse
 from houses.models import House
 from orders.forms import OrderForm
 from django.http import HttpResponseRedirect
+from houses.forms import HousesFilterForm
 
 
 def houses_list(request):
     houses = House.objects.all()  # создаем запрос через ORM, в переменной houses содержаться все дома,
     # которые добавили через админку, эквивалентно SELECT * FROM houses_house ORDER BY name, через модель House
     # получаем все объекты, queryset - набор данных полученных с помощью SQL запроса
-    for house in houses:
-        print(house.name, house.price, house.date)
-    return render(request, "houses/houses_list.html", {"houses": houses})
+    form = HousesFilterForm(request.GET)
+    if form.is_valid():
+        if form.cleaned_data["min_price"]:
+            # price__gte - больше либо равно
+            houses = houses.filter(price__gte=form.cleaned_data["min_price"])
+        if form.cleaned_data["max_price"]:
+            # price__lte - меньше либо равно
+            houses = houses.filter(price__lte=form.cleaned_data["max_price"])
+    return render(request, "houses/houses_list.html", {"houses": houses, "form": form})
 
 
 def house_detail(request, house_id):
